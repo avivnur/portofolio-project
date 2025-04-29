f447ceaa176fbcc782670f8dae16000980bb506a
Aviv Nur <47585222+avivnur@users.noreply.github.com>
2025-04-29 14:40:18 +0000

    finished chapter3

--- chapter3/test_crud.py
+++ chapter3/test_crud.py
@@ -42,8 +42,44 @@ def test_get_new_performances(db_session):
     """Tests that the count od performances is the database is what is expected"""
     performances = crud.get_performances(db_session, skip=0, limit=18000,
                                          min_last_changed_date=test_date)
+    assert len(performances) == 2711
+
+def test_get_league(db_session):
+    """Test you can get a league"""
+    league = crud.get_league(db_session, league_id=5002)
+    assert league.league_id == 5002
+    assert len(league.teams) == 8
+
+def test_get_leagues(db_session):
+    """Tests that the count of leagues in the database is what is expected"""
+    leagues = crud.get_leagues(db_session, skip=0, limit=10000, min_last_changed_date=test_date)
+    assert len(leagues) == 5
+
+def test_get_teams(db_session):
+    """Tests that the count of teams in the database is what is expected"""
+    teams = crud.get_teams(db_session, skip=0, limit=10000, min_last_changed_date=test_date)
+    assert len(teams) == 20
+
+def test_get_teams_for_one_league(db_session):
+    """Tests that the count of teams in the database is what is expected"""
+    teams = crud.get_teams(db_session, league_id=5001)
+    assert len(teams) == 12
+    assert teams[0].league_id == 5001
+
+def test_get_team_players(db_session):
+    """Tests that a team record can retreive players, and that are on the first team"""
+    first_team = crud.get_teams(db_session, skip=0, limit=1000, min_last_changed_date=test_date)[0]
+    assert len(first_team.players) == 7
 
 # test the count functions
 def test_get_player_count(db_session):
     player_count = crud.get_player_count(db_session)
     assert player_count == 1018
+
+def test_get_team_count(db_session):
+    team_count = crud.get_team_count(db_session)
+    assert team_count == 20
+
+def test_get_league_count(db_session):
+    league_count = crud.get_league_count(db_session)
+    assert league_count == 5
